4a34125f66c5b60816c95af5fd3acc00bf382ffd
Vladimir Lopez <vladimir.lopez@wizeline.com>
2019-10-31 18:11:53 -0600

    Add SearchField component

---
+++ src/components/SelectField/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { components } from 'react-select'
+import AsyncSelect from 'react-select/async'
+
+import './SelectField.scss'
+
+const DropdownIndicator = props => {
+  return (
+    <components.DropdownIndicator {...props}>
+      <span className="sp sp-location" />
+    </components.DropdownIndicator>
+  )
+}
+
+const IndicatorSeparator = ({ innerProps }) => <span />
+
+const Option = props => (
+  <components.Option {...props}>
+    <div className="is-flex file level">
+      <p className="is-size-7">{props.label}</p>
+      <span className="sp-location" />
+    </div>
+  </components.Option>
+)
+
+const selectComponents = { DropdownIndicator, IndicatorSeparator, Option }
+
+function SelectField(props) {
+  const { notFoundText, onSearch, onSelect } = props
+
+  return (
+    <AsyncSelect
+      cacheOptions
+      placeholder=""
+      onChange={onSelect}
+      loadOptions={onSearch}
+      className="field"
+      classNamePrefix="search"
+      autoFocus
+      components={selectComponents}
+      noOptionsMessage={() => notFoundText}
+      theme={theme => ({
+        ...theme,
+        colors: {
+          ...theme.colors,
+          primary50: '#0a1421',
+          primary25: '#0a1421',
+          primary: 'transparent',
+          neutral0: '#15202E',
+          neutral90: 'white',
+          neutral70: 'white',
+          neutral80: 'white'
+        }
+      })}
+    />
+  )
+}
+
+export default SelectField
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
-import Textfield from '@sunpower/textfield'
 import { useI18n } from 'shared/i18n'
-import './Home.scss'
 import { paths } from 'routes/paths'
+import SelectField from 'components/SelectField'
+import './Home.scss'
 
 function Home() {
   const t = useI18n()
 
+  const options = [
+    { value: '1', label: '555 Home Street, San Jose, Ca' },
+    { value: '2', label: '555 Home Street, Tulsa, Ok' }
+  ]
+  const notFoundText = t('NOT_FOUND')
+
+  const filterRemote = (inputValue, cb) => {
+    const no = options.filter(i =>
+      i.label.toLowerCase().includes(inputValue.toLowerCase())
+    )
+
+    setTimeout(() => {
+      cb(no)
+    }, 2000)
+  }
+
   return (
     <section className="home is-flex has-text-centered">
       <div className="section">
-        <span className="sp-map has-text-white" />
+        <span className="sp sp-map has-text-white" />
         <h6 className="is-uppercase mt-20 mb-20">{t('SELECT_SITE')}</h6>
-        <form action="">
-          <Textfield input={{}} meta={{}} />
-        </form>
+        <SelectField
+          onSearch={filterRemote}
+          onSelect={console.info}
+          notFoundText={notFoundText}
+        />
       </div>
       <section>
         <p>{t('CS_NOT_FOUND')}</p>
