b48868eb8f1df26f61e44808d55a0516be52e074
Vladimir Lopez <vladimir.lopez@wizeline.com>
2019-11-14 12:48:41 -0600

    Update icon names

--- src/components/Header/menuItems.js
+++ src/components/Header/menuItems.js
@@ -7,17 +7,17 @@ export default [
     to: paths.PROTECTED.ROOT
   },
   {
-    icon: 'sp-map',
+    icon: 'sp-update',
     text: 'MANAGE_FIRMWARES',
     to: paths.PROTECTED.MANAGE_FIRMWARES
   },
   {
-    icon: 'sp-map',
+    icon: 'sp-info',
     text: 'VERSION_INFORMATION',
     to: paths.PROTECTED.VERSION_INFORMATION
   },
   {
-    icon: 'sp-map',
+    icon: 'sp-feedback',
     text: 'GIVE_FEEDBACK',
     to: paths.PROTECTED.GIVE_FEEDBACK
   }
