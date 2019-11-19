21637c73895ebbc5f5684f78cf12cd4142418403
Vladimir Lopez <vladimir.lopez@wizeline.com>
2019-11-19 15:54:43 -0600

    Isolate the Header animation logic in a HOC

--- energylink-connect2-app/src/components/Header/index.jsx
+++ energylink-connect2-app/src/components/Header/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import clsx from 'clsx'
 import isNil from 'ramda/src/isNil'
-import { useSelector } from 'react-redux'
-import { useTransition, animated } from 'react-spring'
 import Logo from '@sunpower/sunpowerimage'
+import withHeaderAnimation from 'hocs/headerAnimation'
 import { trimString } from 'shared/trim'
 import { either } from 'shared/utils'
 import { toggleRoute } from 'shared/routing'
@@ -14,42 +13,33 @@ import './Header.scss'
 const getCount = window => (window.innerWidth > 375 ? 35 : 30)
 const isMenuPath = (history, path) => history.location.pathname === path
 
-const Header = ({ text, icon = 'sp-menu', iconOpen = 'sp-chevron-left' }) => {
-  const showHeader = useSelector(({ ui }) => ui.header)
-  const grow = useTransition(!!showHeader, null, {
-    from: { maxHeight: 0, opacity: 0, marginBottom: '0rem' },
-    enter: { maxHeight: 90, opacity: 1, marginBottom: '1.5rem' },
-    leave: { maxHeight: 0, opacity: 0, marginBottom: '0rem' }
-  })
+export const Header = ({
+  text,
+  icon = 'sp-menu',
+  iconOpen = 'sp-chevron-left'
+}) => {
   const { history } = useRouter()
 
   const menuOpen = isMenuPath(history, paths.PROTECTED.MENU.path)
   const menuIcon = menuOpen ? iconOpen : icon
   const classIcon = clsx('sp', menuIcon, { 'has-text-primary': menuOpen })
 
-  return grow.map(
-    ({ item, props, key }) =>
-      item && (
-        <animated.header
-          className="header is-flex level is-clipper"
-          style={props}
-          key={key}
-        >
-          <span
-            className={classIcon}
-            onClick={toggleRoute(paths.PROTECTED.MENU.path, history)}
-            role="button"
-          />
-          {either(
-            isNil(text),
-            <Logo />,
-            <span className="text has-text-white" title={text}>
-              {trimString(text, getCount(window))}
-            </span>
-          )}
-        </animated.header>
-      )
+  return (
+    <section className="header is-flex level is-clipper">
+      <span
+        className={classIcon}
+        onClick={toggleRoute(paths.PROTECTED.MENU.path, history)}
+        role="button"
+      />
+      {either(
+        isNil(text),
+        <Logo />,
+        <span className="text has-text-white" title={text}>
+          {trimString(text, getCount(window))}
+        </span>
+      )}
+    </section>
   )
 }
 
-export default Header
+export default withHeaderAnimation(Header)
