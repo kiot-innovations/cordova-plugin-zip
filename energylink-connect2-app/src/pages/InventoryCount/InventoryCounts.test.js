537853e98c204ea41504aa4f25a6e911bf81b177
Fernando Cervantes <fernando.cervantes@wizeline.com>
2019-11-21 16:17:44 -0600

    Changed buttons copy

--- energylink-connect2-app/src/pages/InventoryCount/InventoryCounts.test.js
+++ energylink-connect2-app/src/pages/InventoryCount/InventoryCounts.test.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import InventoryCount from '.'
 import * as i18n from 'shared/i18n'
+import * as reactRedux from 'react-redux'
 
 describe('Inventory Count page', () => {
+  let dispatchMock
+
   beforeEach(() => {
+    dispatchMock = jest.fn()
+    jest.spyOn(reactRedux, 'useDispatch').mockImplementation(() => dispatchMock)
     jest
       .spyOn(i18n, 'useI18n')
       .mockImplementation(path => (key, ...params) =>
