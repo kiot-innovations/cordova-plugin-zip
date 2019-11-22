bd30d00950600f947fc6afd8aead6a0ebe484eb3
Esteban Trillo <esteban.trillo@wizeline.com>
2019-11-22 16:06:56 -0600

    google map will match the address.
    Had to make a bunch of weird logic to make the google maps work gracefully with react-final-form-hook but at the end, we have a console.log when we click submit

--- energylink-connect2-app/src/shared/utils.js
+++ energylink-connect2-app/src/shared/utils.js
@@ -1,2 +1,19 @@
 export const either = (condition, whenTrue, whenFalse) =>
   condition ? whenTrue : whenFalse
+
+/**
+ * Will return the data of the placed found with google maps
+ * @param address
+ * @returns {Promise<{address_components, geometry}>}
+ */
+export function geocodeByAddress(address) {
+  const geocoder = new window.google.maps.Geocoder()
+  const OK = window.google.maps.GeocoderStatus.OK
+
+  return new Promise(function(resolve, reject) {
+    geocoder.geocode({ address: address }, function(results, status) {
+      if (status !== OK) reject(status)
+      resolve(results[0])
+    })
+  })
+}
