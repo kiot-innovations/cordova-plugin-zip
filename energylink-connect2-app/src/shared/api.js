d7801841c91b2be44c4439dd0dc64c22f4f7810e
Vladimir Lopez <vladimir.lopez@wizeline.com>
2019-11-27 16:23:41 -0600

    Put API Endpoints in .env files

--- energylink-connect2-app/src/shared/api.js
+++ energylink-connect2-app/src/shared/api.js
@@ -3,33 +3,23 @@ import Swagger from 'swagger-client'
 export let apiAuth, apiDevice, apiParty, apiSearch, apiSite
 
 export default () => {
-  Swagger('http://consolemock.p2e.io:10011/v1/edp/swagger/edp-api-auth').then(
-    client => {
-      apiAuth = client
-    }
-  )
+  Swagger(process.env.REACT_APP_SWAGGER_AUTH).then(client => {
+    apiAuth = client
+  })
 
-  Swagger('http://consolemock.p2e.io:10011/v1/edp/swagger/edp-api-device').then(
-    client => {
-      apiDevice = client
-    }
-  )
+  Swagger(process.env.REACT_APP_SWAGGER_DEVICE).then(client => {
+    apiDevice = client
+  })
 
-  Swagger('http://consolemock.p2e.io:10011/v1/edp/swagger/edp-api-party').then(
-    client => {
-      apiParty = client
-    }
-  )
+  Swagger(process.env.REACT_APP_SWAGGER_PARTY).then(client => {
+    apiParty = client
+  })
 
-  Swagger('http://consolemock.p2e.io:10011/v1/edp/swagger/edp-api-search').then(
-    client => {
-      apiSearch = client
-    }
-  )
+  Swagger(process.env.REACT_APP_SWAGGER_SEARCH).then(client => {
+    apiSearch = client
+  })
 
-  Swagger('http://consolemock.p2e.io:10011/v1/edp/swagger/edp-api-site').then(
-    client => {
-      apiSite = client
-    }
-  )
+  Swagger(process.env.REACT_APP_SWAGGER_SITE).then(client => {
+    apiSite = client
+  })
 }
