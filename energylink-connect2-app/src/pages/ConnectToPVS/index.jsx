fefa733f0cbd46cd1f1b3baee8706c688a2ed68e
Fernando Cervantes <fernando.cervantes@wizeline.com>
2019-12-03 10:50:21 -0600

    Modified page a bit

--- energylink-connect2-app/src/pages/ConnectToPVS/index.jsx
+++ energylink-connect2-app/src/pages/ConnectToPVS/index.jsx
@@ -1,31 +1,15 @@
 import React from 'react'
 import { useI18n } from 'shared/i18n'
-import QrReader from 'react-qr-reader'
 
 function ConnectToPVS() {
   const t = useI18n()
-
-  const handleScan = data => {
-    alert(data)
-  }
-
-  const handleError = err => {
-    console.error(err)
-  }
 
   return (
     <div className="is-vertical has-text-centered">
       <span className="is-uppercase has-text-weight-bold mb-40">
         {t('LOOK_FOR_QR')}
       </span>
-      <div className="cam-viewfinder">
-        <QrReader
-          delay={1000}
-          onScan={handleScan}
-          onError={handleError}
-          style={{ width: '100%' }}
-        />
-      </div>
+      <div className="cam-viewfinder" />
     </div>
   )
 }
