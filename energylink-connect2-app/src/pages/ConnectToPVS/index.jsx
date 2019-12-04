b4d2c416f2995b649ce1753237f3368e0086cdda
Fernando Cervantes <fernando.cervantes@wizeline.com>
2019-12-04 11:54:51 -0600

    Fixed index file

--- energylink-connect2-app/src/pages/ConnectToPVS/index.jsx
+++ energylink-connect2-app/src/pages/ConnectToPVS/index.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
+import ExampleImage from './assets/example.png'
 import { useI18n } from 'shared/i18n'
+import { scanBarcodes } from '../../shared/utils'
 
 function ConnectToPVS() {
   const t = useI18n()
+
+  const onSuccess = data => {
+    alert(data.text)
+  }
+
+  const onFail = err => {
+    alert(err)
+  }
 
   return (
     <div className="is-vertical has-text-centered">
       <span className="is-uppercase has-text-weight-bold mb-40">
         {t('LOOK_FOR_QR')}
       </span>
-      <div className="cam-viewfinder" />
+      <div className="example-image mt-20 mb-20">
+        <span>{t('EXAMPLE_IMAGE')}</span>
+        <img className="mt-15" src={ExampleImage} />
+      </div>
+      <button
+        className="button is-primary"
+        onClick={() => scanBarcodes(onSuccess, onFail)}
+      >
+        {t('START_SCAN')}
+      </button>
     </div>
   )
 }
