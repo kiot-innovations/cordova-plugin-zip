ec2946fd4c2645f54f94206bf32b4b6d5c2dff19
Fernando Cervantes <fernando.cervantes@wizeline.com>
2019-12-05 15:47:50 -0600

    Added QR Code Decryption

--- energylink-connect2-app/src/pages/ConnectToPVS/index.jsx
+++ energylink-connect2-app/src/pages/ConnectToPVS/index.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ExampleImage from './assets/example.png'
 import { useI18n } from 'shared/i18n'
 import { scanBarcodes } from '../../shared/utils'
+import CryptoJS from 'crypto-js'
 
 function ConnectToPVS() {
   const t = useI18n()
 
   const onSuccess = data => {
-    alert(data.text)
+    let trimData = data.replace(/\s+/g, '')
+    let QRdata = CryptoJS.AES.decrypt(trimData, process.env.REACT_APP_WIFIKEY)
+    let wifiData = QRdata.toString(CryptoJS.enc.Utf8)
+    alert(wifiData)
   }
 
   const onFail = err => {
@@ -21,7 +25,7 @@ function ConnectToPVS() {
       </span>
       <div className="example-image mt-20 mb-20">
         <span>{t('EXAMPLE_IMAGE')}</span>
-        <img className="mt-15" src={ExampleImage} />
+        <img className="mt-15" src={ExampleImage} alt="PVS QR code example" />
       </div>
       <button
         className="button is-primary"
