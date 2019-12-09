2d368b87c62677e3d0b607e7488c6287ee1c4a38
Fernando Cervantes <fernando.cervantes@wizeline.com>
2019-12-09 15:20:51 -0800

    Removed unnecesary asnyc/await clauses

--- energylink-connect2-app/src/state/actions/network.js
+++ energylink-connect2-app/src/state/actions/network.js
@@ -5,10 +5,10 @@ export const PVS_CONNECTION_SUCCESS = createAction('PVS_CONNECTION_SUCCESS')
 export const PVS_CONNECTION_ERROR = createAction('PVS_CONNECTION_ERROR')
 
 export const connectTo = (ssid, password) => {
-  return async dispatch => {
+  return dispatch => {
     dispatch(PVS_CONNECTION_INIT())
     if (window.device.platform === 'iOS') {
-      await window.WifiWizard2.iOSConnectNetwork(ssid, password)
+      window.WifiWizard2.iOSConnectNetwork(ssid, password)
         .then(() => {
           dispatch(PVS_CONNECTION_SUCCESS(ssid))
         })
