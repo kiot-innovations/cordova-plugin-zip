9a98608920fa56093388cb7e14afa32d07a84ce9
Vladimir Lopez <vladimir.lopez@wizeline.com>
2019-12-10 01:24:21 -0600

    Implement login actions and reducers

--- energylink-connect2-app/src/state/actions/auth.js
+++ energylink-connect2-app/src/state/actions/auth.js
@@ -1,9 +1,10 @@
 import { createAction } from 'redux-act'
-import { getUser } from './user'
-import { httpPost, httpDelete, httpGet } from '../../shared/fetch'
+import { equals, head, prop, propOr } from 'ramda'
+import { httpPost, httpGet } from '../../shared/fetch'
 import { trimObject } from '../../shared/trim'
 import { fetchInventory } from './inventory'
 import { getFile } from './fileDownloader'
+import authClient from 'shared/auth/sdk'
 
 export const WORKFLOW_TYPES = {
   SELF: 1,
@@ -14,23 +15,115 @@ export const WORKFLOW_TYPES = {
 export const LOGIN_INIT = createAction('LOGIN_INIT')
 export const LOGIN_SUCCESS = createAction('LOGIN_SUCCESS')
 export const LOGIN_ERROR = createAction('LOGIN_ERROR')
+export const SET_TOKEN = createAction('SET_TOKEN')
+export const LOGOUT = createAction('LOGOUT')
 
-export const performLogin = values => {
-  return async dispatch => {
+export const requestLogin = () => {
+  return dispatch => {
     try {
-      dispatch(LOGIN_INIT())
-      const response = await httpPost('/authenticate', values)
-      if (response.status !== 200) {
-        dispatch(LOGIN_ERROR(response))
-        return
-      }
-      dispatch(LOGIN_SUCCESS(response.data))
-      dispatch(getUser())
-      dispatch(fetchInventory())
-      dispatch(getFile())
+      let state = authClient.generateRandomValue()
+      let nonce = authClient.generateRandomValue()
+
+      dispatch(LOGIN_INIT({ state, nonce }))
+      /// this triggers Ping auth service
+      authClient.authorize(state, nonce)
+    } catch (err) {
+      dispatch(LOGIN_ERROR({ message: err }))
+    }
+  }
+}
+
+export const handleLoginFromPing = URL => {
+  return (dispatch, getState) => {
+    const { user } = getState()
+    const hashes = authClient.parseHash(URL)
+
+    if (hashes.error && hashes.error_description) {
+      const error = `${hashes.error}: ${hashes.error_description}`
+      dispatch(LOGIN_ERROR({ message: error }))
+      return
+    }
+
+    const [, stateMatch] = URL.match('[?#&]state=([^&]*)') || []
+    if (stateMatch && !equals(stateMatch, user.auth.state)) {
+      dispatch(LOGIN_ERROR({ message: 'State parameter mismatch' }))
+      return
+    }
+
+    const [, codeMatch] = URL.match('[?#&]code=([^&]*)') || []
+    if (hashes.access_token) {
+      dispatch(handleUserProfile(hashes.access_token))
+    } else if (codeMatch) {
+      authClient
+        .getAccessToken(codeMatch)
+        .then(token => {
+          dispatch(handleUserProfile(token.access_token, token.id_token))
+        })
+        .catch(error => {
+          dispatch(LOGIN_ERROR({ message: 'Could not get access token' }))
+        })
+    }
+
+    if (hashes.id_token) {
+      dispatch(verifyToken(hashes.id_token))
+    }
+  }
+}
+
+export const handleUserProfile = (access_token, id_token) => {
+  return dispatch => {
+    try {
+      authClient
+        .getUserInfo(access_token)
+        .then(user => {
+          const payload = { data: user, auth: { access_token } }
+          dispatch(LOGIN_SUCCESS(payload))
+          dispatch(fetchInventory())
+          dispatch(getFile())
+          if (id_token) {
+            dispatch(SET_TOKEN(id_token))
+          }
+        })
+        .catch(error => {
+          const details = propOr([], 'details', error)
+          const firstError = head(details)
+          const firstErrorCode = prop('code', firstError)
+
+          if (equals(firstErrorCode, 'INVALID_VALUE')) {
+            const firstErrorMsg = prop('message', firstError)
+            const ERROR = firstErrorMsg.includes('Access token expired')
+              ? 'ACCESS_TOKEN_EXPIRED'
+              : firstErrorMsg
+            dispatch(LOGIN_ERROR({ message: ERROR }))
+          } else {
+            const description = prop('error_description', error)
+            dispatch(LOGIN_ERROR({ message: description }))
+          }
+        })
     } catch (err) {
       dispatch(LOGIN_ERROR(err))
     }
+  }
+}
+
+export const verifyToken = id_token => {
+  return (dispatch, getState) => {
+    const { user } = getState()
+    authClient
+      .verifyIdToken(id_token, {
+        nonce: user.auth.nonce
+      })
+      .catch(error => {
+        dispatch(LOGIN_ERROR({ message: 'INVALID_TOKEN' }))
+        dispatch(LOGOUT())
+      })
+  }
+}
+
+export const logout = () => {
+  return (dispatch, getState) => {
+    const { user } = getState()
+    authClient.signOff(user.auth.id_token, user.auth.state)
   }
 }
 
@@ -58,16 +151,6 @@ export const createAccount = values => {
     } catch (err) {
       dispatch(CREATE_ACCOUNT_ERROR(err))
     }
-  }
-}
-
-export const LOGOUT = createAction('LOGOUT')
-
-export const logout = () => {
-  return async (dispatch, getState) => {
-    const state = getState()
-    await httpDelete('/session', state)
-    dispatch(LOGOUT())
   }
 }
 
@@ -103,7 +186,7 @@ export const resetPassword = (values, pkg) => {
       }
       dispatch(RESET_PASSWORD_SUCCESS())
       dispatch(
-        performLogin(
+        requestLogin(
           trimObject({
             username: values.email,
             password: values.password
@@ -132,19 +215,10 @@ export const VALIDATE_SESSION_SUCCESS = createAction('VALIDATE_SESSION_SUCCESS')
 export const VALIDATE_SESSION_ERROR = createAction('VALIDATE_SESSION_ERROR')
 
 export const validateSession = () => {
-  return async (dispatch, getState) => {
-    try {
-      const state = getState()
-      dispatch(VALIDATE_SESSION_INIT())
-      const response = await httpGet('/session', state)
-      if (response.status !== 200) {
-        dispatch(VALIDATE_SESSION_ERROR(response))
-        dispatch(LOGOUT())
-        return
-      }
-      dispatch(VALIDATE_SESSION_SUCCESS())
-    } catch (err) {
-      dispatch(VALIDATE_SESSION_ERROR(err))
+  return (dispatch, getState) => {
+    const { user } = getState()
+    if (user.auth.id_token) {
+      dispatch(verifyToken(user.auth.id_token))
     }
   }
 }
--- energylink-connect2-app/src/state/reducers/user/index.js
+++ energylink-connect2-app/src/state/reducers/user/index.js
@@ -4,6 +4,7 @@ import {
   LOGIN_INIT,
   LOGOUT,
   LOGIN_ERROR,
+  SET_TOKEN,
   VALIDATE_SESSION_INIT,
   VALIDATE_SESSION_SUCCESS,
   VALIDATE_SESSION_ERROR
@@ -25,21 +26,27 @@ const initialState = {
 
 export const userReducer = createReducer(
   {
-    [LOGIN_INIT]: state => ({
+    [LOGIN_INIT]: (state, payload) => ({
       ...state,
-      auth: {},
-      isAuthenticating: true
+      isAuthenticating: true,
+      auth: { ...state.auth, state: payload.state, nonce: payload.nonce }
     }),
     [LOGIN_SUCCESS]: (state, payload) => ({
       ...state,
-      auth: { ...payload },
+      auth: { ...state.auth, ...payload.auth },
+      data: { ...payload.data },
       isAuthenticating: false
     }),
     [LOGIN_ERROR]: (state, payload) => ({
       ...state,
-      err: { ...payload },
-      isAuthenticating: false
+      initialState,
+      err: { ...payload }
     }),
+    [SET_TOKEN]: (state, payload) => ({
+      ...state,
+      auth: { ...state.auth, id_token: payload }
+    }),
+
     [GET_USER_INIT]: state => ({ ...state, data: {} }),
     [GET_USER_SUCCESS]: (state, payload) => ({
       ...state,
