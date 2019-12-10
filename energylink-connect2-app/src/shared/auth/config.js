ccb1d3972f91f6665147a8563d999767c3bd0982
Vladimir Lopez <vladimir.lopez@wizeline.com>
2019-12-10 01:22:12 -0600

    Use config values from the ENV files

--- energylink-connect2-app/src/shared/auth/config.js
+++ energylink-connect2-app/src/shared/auth/config.js
@@ -3,19 +3,19 @@ export default {
    * Application authorization details. For more information check "Getting Started" in README.md
    */
   // Specifies the environment’s UUID.
-  environmentId: 'd8f71286-f3d9-4018-b62e-8d5b80914084',
+  environmentId: process.env.REACT_APP_PING_ENVIRONMENT,
   // Specifies the code or token type returned by an authorization request. Options are token, id_token, and code. Default values is "token id_token". This is a required property.
   responseType: 'token id_token',
   // Specifies the application’s UUID.
-  clientId: 'fdc90021-5951-4225-8f24-7b77f764beaf',
+  clientId: process.env.REACT_APP_PING_CLIENT_ID,
   // Specifies the application’s secret key
   clientSecret: '',
   // Specifies the grant type of the token request. Options are authorization_code, implicit, and client_credentials
   grantType: 'implicit',
   // Specifies the URL that specifies the return entry point of the application. This is a required property.
-  redirectUri: 'elc2://auth',
+  redirectUri: process.env.REACT_APP_PING_REDIRECT_URI,
   // Specifies an optional parameter that specifies the URL to which the browser is redirected after a logout has been performed.
-  logoutRedirectUri: 'http://localhost:8080',
+  logoutRedirectUri: process.env.REACT_APP_PING_REDIRECT_URI,
   // Specifies permissions that determine the resources that the application can access. This parameter is not required, but it is needed to specify accessible resources.
   scope: 'profile address email phone',
   // Specifies whether the user is prompted to login for re-authentication. The prompt parameter can be used as a way to check for existing authentication, verifying that the user is still present for the current session. For prompt=none, the user is never prompted to login to re-authenticate, which can result in an error if authentication is required. For prompt=login, if time since last login is greater than the max-age, then the current session is stashed away in the flow state and treated in the flow as if there was no previous existing session. When the flow completes, if the flow’s user is the same as the user from the stashed away session, the stashed away session is updated with the new flow data and persisted (preserving the existing session ID). If the flow’s user is not the same as the user from the stashed away session, the stashed away session is deleted (logout) and the new session is persisted.
