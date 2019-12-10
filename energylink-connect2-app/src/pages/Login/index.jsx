d473fdc6505fd16a58908b5fc3dbef2c4b149e7a
Vladimir Lopez <vladimir.lopez@wizeline.com>
2019-12-10 01:25:24 -0600

    Update Login Page

--- energylink-connect2-app/src/pages/Login/index.jsx
+++ energylink-connect2-app/src/pages/Login/index.jsx
@@ -1,131 +1,73 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import clsx from 'clsx'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { useForm, useField } from 'react-final-form-hooks'
 
 import Logo from '@sunpower/sunpowerimage'
-import TextField from '@sunpower/textfield'
-import PasswordToggle from 'components/PasswordToggle'
 
-import { LOGIN_INIT } from 'state/actions/auth'
-import { trimObject } from 'shared/trim'
+import { requestLogin, handleLoginFromPing } from 'state/actions/auth'
 import { useI18n } from 'shared/i18n'
 import paths from 'routes/paths'
 
 import './Login.scss'
 
-function onSubmit(dispatch) {
-  return values => dispatch(LOGIN_INIT(trimObject(values)))
-}
-
-function validate(values, t) {
-  const errors = {}
-  if (!values.username) {
-    errors.username = t('REQ_USERNAME')
-  }
-  if (!values.password) {
-    errors.password = t('REQ_PASSWORD')
-  }
-  return errors
-}
-
 function Login() {
   const t = useI18n()
   const dispatch = useDispatch()
-  const { form, handleSubmit } = useForm({
-    onSubmit: onSubmit(dispatch),
-    validate: values => validate(values, t)
-  })
-  const username = useField('username', form)
-  const password = useField('password', form)
-  const isPersistent = useField('isPersistent', form)
-  const err = useSelector(state => state.user.err)
-  const submitting = useSelector(state => state.user.isAuthenticating)
+
+  const auth = useSelector(state => state.user.auth)
+  const submitting = useSelector(state => state.user.submitting)
 
   const loginClassName = clsx('button', 'is-primary', {
     'is-loading': submitting
   })
 
-  const ROUTES = {
-    FORGOT: { pathname: paths.UNPROTECTED.FORGOT_PASSWORD.path },
-    ASSISTANCE: { pathname: paths.UNPROTECTED.GET_ASSISTANCE.path }
-  }
+  useEffect(() => {
+    window.handleOpenURL = handleOpenURL(dispatch)
+  }, [dispatch])
 
   return (
-    <section className="login section full-min-height is-flex space-around pt-0">
-      <div className="container">
+    <section className="login section full-min-height is-flex">
+      <div className="container has-text-centered pb-20">
         <Logo />
-        <h1 className="is-uppercase has-text-white heading">
-          EnergyLink Connect
-        </h1>
+        <h1 className="is-uppercase has-text-white heading">{t('APP_NAME')}</h1>
       </div>
 
-      <form className="control" onSubmit={handleSubmit}>
-        <div className="container full-height has-text-centered pb-20">
-          <div>
-            <TextField
-              input={username.input}
-              meta={username.meta}
-              placeholder={t('PLACEHOLDER_EMAIL')}
-              type="email"
-              autoComplete="email"
-            />
-            <PasswordToggle
-              input={password.input}
-              meta={password.meta}
-              placeholder={t('PLACEHOLDER_PASSWORD')}
-              autoComplete="current-password"
-            />
-            <div className="field is-dark">
-              <p className="control rememeber-me">
-                <label className="checkbox">
-                  <input {...isPersistent.input} type="checkbox" />
-                  {t('REMEMBER_ME')}
-                </label>
-              </p>
-            </div>
-          </div>
+      <div className="auto">
+        <div className="mb-30 has-text-centered">
+          <p>{t('NO_ACCOUNT')}</p>
+          <Link className="link" to={paths.UNPROTECTED.GET_ASSISTANCE.path}>
+            {t('GET_ASSISTANCE')}
+          </Link>
+        </div>
 
-          <div className="has-text-centered forgot">
-            <Link className="link" to={ROUTES.FORGOT}>
-              {t('FORGOT_PASSWORD')}
-            </Link>
-            {err && err.status ? (
-              <p id="error-message" className="error-message mt-20">
-                {t('WRONG_CREDENTIALS')}
-              </p>
-            ) : (
-              ''
-            )}
-          </div>
-
-          <div>
-            <div className="mb-30">
-              <p>{t('NO_ACCOUNT')}</p>
-              <Link className="link" to={ROUTES.ASSISTANCE}>
-                {t('GET_ASSISTANCE')}
-              </Link>
-            </div>
-
-            <div className="mt-10">
-              <div className="field is-grouped is-grouped-centered">
-                <p className="control">
-                  <button
-                    className={loginClassName}
-                    type="submit"
-                    disabled={!!submitting}
-                  >
-                    {t('BTN_LOGIN')}
-                  </button>
-                </p>
-              </div>
-            </div>
+        <div className="mt-10">
+          <div className="field is-grouped is-grouped-centered">
+            <p className="control">
+              <button
+                type="button"
+                className={loginClassName}
+                disabled={!!submitting}
+                onClick={onSubmit(dispatch)}
+              >
+                {!auth.access_token ? t('BTN_LOGIN') : t('BTN_LOGOUT')}
+              </button>
+            </p>
           </div>
         </div>
-      </form>
+      </div>
     </section>
   )
 }
 
 export default Login
+
+const onSubmit = dispatch => () => {
+  dispatch(requestLogin())
+}
+
+const handleOpenURL = dispatch => URL => {
+  setTimeout(() => {
+    dispatch(handleLoginFromPing(URL))
+  }, 0)
+}
