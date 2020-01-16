ee157511564bc7818becb03cfe5eda3222a7c7d1
Esteban Trillo <esteban@devtrillo.com>
2020-01-16 12:48:24 -0600

    Added internationalization

--- energylink-connect2-app/src/pages/InstallSuccess/index.jsx
+++ energylink-connect2-app/src/pages/InstallSuccess/index.jsx
@@ -1,30 +1,26 @@
 import React from 'react'
 import './InstallSuccess.scss'
-
+import { useI18n } from 'shared/i18n'
 const InstallSuccessful = props => {
+  const t = useI18n()
   return (
-    <div className="file level has-text-centered fill-parent install-success-screen">
+    <div className="file level has-text-centered fill-parent install-success-screen auto">
       <div className="is-vertical file level is-vertical is-flex tile">
         <span className="is-uppercase has-text-weight-bold mb-25 mt-25 ">
-          install successful
+          {t('INSTALL_SUCCESS')}
         </span>
         <span className="sp-pvs has-text-white mb-30" />
-        <span className="mb-20">
-          Please take a picture for your records of the module layout diagram
-          before leaving the site
-        </span>
+        <span className="mb-20">{t('INSTALL_SUBTITLE')}</span>
         <span className="has-text-white">
-          You can now{' '}
-          <span className="has-text-weight-bold">
-            turn off the solar breakers
-          </span>
+          {`${t('YOU_CAN')} `}
+          <span className="has-text-weight-bold">{t('TURN_OF_SOLAR')}</span>
         </span>
         <div className="is-flex auto is-vertical tile">
           <button className="button is-primary is-uppercase is-center mt-50">
-            Configure
+            {t('CONFIGURE')}
           </button>
           <button className="configure-button is-uppercase is-center mt-50 has-text-weight-bold">
-            not now
+            {t('NOT_NOW')}
           </button>
         </div>
       </div>
