d2964e7bed35d5b83bf1eff718fe3ae7fc9092a2
Fernando Cervantes <fernando.cervantes@wizeline.com>
2020-01-21 12:51:06 -0600

    Removed unneeded merge function

--- energylink-connect2-app/src/state/epics/pvs/pvsScanEpic.js
+++ energylink-connect2-app/src/state/epics/pvs/pvsScanEpic.js
@@ -6,16 +6,6 @@ import { postBinary } from 'shared/fetch'
 import { eqBy, prop, unionWith } from 'ramda'
 import { b64toBlob } from '../../../shared/utils'
 
-function mergeSN(arr1, arr2) {
-  if (arr1.length > 0) {
-    const merge = unionWith(eqBy(prop('serial_number')))
-    const mergedData = merge(arr1, arr2)
-    return mergedData
-  } else {
-    return arr2
-  }
-}
-
 export const pvsScanEpic = (action$, state$) => {
   let state
   state$.subscribe(s => {
@@ -31,7 +21,11 @@ export const pvsScanEpic = (action$, state$) => {
         map(response => {
           return response.devices && response.devices.length > 0
             ? pvsActions.GET_SN_SUCCESS(
-                mergeSN(state.pvs.serialNumbers, response.devices)
+                unionWith(
+                  eqBy(prop('serial_number')),
+                  state.pvs.serialNumbers,
+                  response.devices
+                )
               )
             : pvsActions.GET_SN_ERROR('NOT_FOUND')
         }),
