2e01dac7887fb5ecb14754a5aa28ea2003f41d12
Vladimir Lopez <vladimir.lopez@wizeline.com>
2020-01-29 12:48:13 -0600

    Add tests

---
+++ energylink-connect2-app/src/pages/SystemConfiguration/NetworkWidget.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import * as reactRedux from 'react-redux'
+import * as i18n from 'shared/i18n'
+import NetworkWidget from './NetworkWidget'
+
+describe('Network Widget', () => {
+  let dispatchMock
+
+  let initialState = {
+    systemConfiguration: {
+      network: {
+        aps: [{ ssid: '1' }, { ssid: '2' }],
+        selectedAP: null,
+        error: null
+      }
+    }
+  }
+
+  beforeEach(() => {
+    dispatchMock = jest.fn()
+    jest.spyOn(reactRedux, 'useDispatch').mockImplementation(() => dispatchMock)
+    jest
+      .spyOn(i18n, 'useI18n')
+      .mockImplementation(path => (key, ...params) =>
+        `${key.toUpperCase()} ${params.join('_')}`.trim()
+      )
+  })
+
+  test('renders correctly', () => {
+    const { component } = mountWithProvider(<NetworkWidget />)(initialState)
+    expect(component).toMatchSnapshot()
+  })
+})
