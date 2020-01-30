f7ab8a5fcca70e96508f87eb4b723eb105cc2697
Vladimir Lopez <vladimir.lopez@wizeline.com>
2020-01-30 12:41:31 -0600

    Add Storage and RSE Widgets Add tests

---
+++ energylink-connect2-app/src/pages/SystemConfiguration/RSEWidget.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { compose, prop } from 'ramda'
+import { useI18n } from 'shared/i18n'
+import { either } from 'shared/utils'
+
+import Collapsible from 'components/Collapsible'
+import SelectField from 'components/SelectField'
+
+const RSE = <span className="sp-info file level mr-15 is-size-4" />
+
+function RSEWidget() {
+  const t = useI18n()
+
+  const [rse, setRSE] = useState(null)
+
+  const RSES = [
+    { label: t('ON'), value: true },
+    { label: t('OFF'), value: false }
+  ]
+
+  return (
+    <div className="pb-15">
+      <Collapsible title={t('RSE')} icon={RSE}>
+        <div className="field is-horizontal mb-15">
+          <div className="field-label">
+            <label htmlFor="siteName" className="label has-text-white">
+              {t('REMOTE_SYSTEM_ENERGYZE')}
+            </label>
+          </div>
+          <div className="field-body">
+            <div className="field">
+              <div className="control">
+                <SelectField
+                  isSearchable={false}
+                  useDefaultDropDown
+                  options={RSES}
+                  defaultValue={{ label: t('OFF'), value: false }}
+                  onSelect={compose(setRSE, prop('value'))}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {either(
+          rse,
+
+          <article>
+            <p className="text-center">{t('RSE_ON_SYSTEM')}</p>
+            <ul>
+              <li>{t('RSE_ON_1')}</li>
+              <li>{t('RSE_ON_2')}</li>
+            </ul>
+
+            <p className="text-center mt-10">{t('RSE_ON_INVERTERS')}</p>
+            <ul>
+              <li>{t('RSE_ON_3')}</li>
+              <li>{t('RSE_ON_4')}</li>
+            </ul>
+          </article>,
+
+          <p className="text-center">{t('RSE_OFF')}</p>
+        )}
+
+        <div className="is-flex mt-15">
+          <button className="button is-primary auto" disabled={rse === null}>
+            {t('APPLY')}
+          </button>
+        </div>
+      </Collapsible>
+    </div>
+  )
+}
+export default RSEWidget
--- energylink-connect2-app/src/pages/SystemConfiguration/index.jsx
+++ energylink-connect2-app/src/pages/SystemConfiguration/index.jsx
@@ -1,21 +1,15 @@
-import React, { useState } from 'react'
-import { prop, compose } from 'ramda'
+import React from 'react'
 import { useI18n } from 'shared/i18n'
-import { either } from 'shared/utils'
-import Collapsible from 'components/Collapsible'
-import SelectField from 'components/SelectField'
 import NetworkWidget from './NetworkWidget'
 import MetersWidget from './MetersWidget'
 import GridBehaviorWidget from './GridBehaviorWidget'
+import StorageWidget from './StorageWidget'
+import RSEWidget from './RSEWidget'
 
 import './SystemConfiguration.scss'
 
-const STI = <span className="sp-battery file level mr-15 is-size-4" />
-const RSE = <span className="sp-info file level mr-15 is-size-4" />
-
 function SystemConfiguration() {
   const t = useI18n()
-  const [rse, setRSE] = useState(null)
 
   return (
     <div className="fill-parent is-flex tile is-vertical has-text-centered system-config pl-10 pr-10">
@@ -24,111 +18,10 @@ function SystemConfiguration() {
       </span>
 
       <GridBehaviorWidget />
-
       <MetersWidget />
-
-      <div className="pb-15">
-        <Collapsible title={t('STORAGE')} icon={STI}>
-          <section className="box is-flex space-around has-background-black has-text-grey pl-10 pr-10">
-            <article>
-              <h6 className="is-size-5 has-text-white">4 batteries</h6>
-              <ul>
-                <li>30849</li>
-                <li>30849</li>
-                <li>30849</li>
-                <li>30849</li>
-              </ul>
-            </article>
-
-            <article>
-              <h6 className="is-size-5 has-text-white">Inverter</h6>
-              <ul>
-                <li>00001B3E9C74</li>
-              </ul>
-
-              <h6 className="is-size-5 has-text-white mt-20">
-                Inverter Gateway
-              </h6>
-              <ul>
-                <li>Installed</li>
-              </ul>
-            </article>
-          </section>
-
-          <section className="box is-flex space-around has-background-black has-text-grey pl-10 pr-10">
-            <article>
-              <h6 className="is-size-5 has-text-white">2 batteries</h6>
-              <ul>
-                <li>30849</li>
-                <li>30849</li>
-              </ul>
-            </article>
-
-            <article>
-              <h6 className="is-size-5 has-text-white">Inverter</h6>
-              <ul>
-                <li>00001B3E9C74</li>
-              </ul>
-            </article>
-          </section>
-        </Collapsible>
-      </div>
-
+      <StorageWidget />
       <NetworkWidget />
-
-      <div className="pb-15">
-        <Collapsible title={t('RSE')} icon={RSE}>
-          <div className="field is-horizontal mb-15">
-            <div className="field-label">
-              <label htmlFor="siteName" className="label has-text-white">
-                {t('REMOTE_SYSTEM_ENERGYZE')}
-              </label>
-            </div>
-            <div className="field-body">
-              <div className="field">
-                <div className="control">
-                  <SelectField
-                    isSearchable={false}
-                    useDefaultDropDown
-                    options={[
-                      { label: t('ON'), value: true },
-                      { label: t('OFF'), value: false }
-                    ]}
-                    defaultValue={{ label: t('OFF'), value: false }}
-                    onSelect={compose(setRSE, prop('value'))}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {either(
-            rse,
-
-            <article>
-              <p className="text-center">{t('RSE_ON_SYSTEM')}</p>
-              <ul>
-                <li>{t('RSE_ON_1')}</li>
-                <li>{t('RSE_ON_2')}</li>
-              </ul>
-
-              <p className="text-center mt-10">{t('RSE_ON_INVERTERS')}</p>
-              <ul>
-                <li>{t('RSE_ON_3')}</li>
-                <li>{t('RSE_ON_4')}</li>
-              </ul>
-            </article>,
-
-            <p className="text-center">{t('RSE_OFF')}</p>
-          )}
-
-          <div className="is-flex mt-15">
-            <button className="button is-primary auto" disabled={rse === null}>
-              {t('APPLY')}
-            </button>
-          </div>
-        </Collapsible>
-      </div>
+      <RSEWidget />
     </div>
   )
 }
