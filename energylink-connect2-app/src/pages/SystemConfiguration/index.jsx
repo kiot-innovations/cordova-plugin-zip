af863c3c4bfa438707062c43225ec2ef393a7d44
Fernando Cervantes <fernando.cervantes@wizeline.com>
2020-02-04 10:54:13 -0600

    WIP for System Config

--- energylink-connect2-app/src/pages/SystemConfiguration/index.jsx
+++ energylink-connect2-app/src/pages/SystemConfiguration/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useI18n } from 'shared/i18n'
+import { useSelector } from 'react-redux'
 import NetworkWidget from './NetworkWidget'
 import MetersWidget from './MetersWidget'
 import GridBehaviorWidget from './GridBehaviorWidget'
@@ -10,6 +11,32 @@ import './SystemConfiguration.scss'
 
 function SystemConfiguration() {
   const t = useI18n()
+  const { selectedOptions } = useSelector(
+    state => state.systemConfiguration.gridBehavior
+  )
+  const { isConnected } = useSelector(
+    state => state.systemConfiguration.network
+  )
+  const { productionCT, consumptionCT, ratedCurrent } = useSelector(
+    state => state.systemConfiguration.meter
+  )
+
+  const submitConfig = () => {
+    try {
+      const configObject = {
+        gridProfile: selectedOptions.profile.id,
+        exportLimit: selectedOptions.exportLimit,
+        gridVoltage: selectedOptions.gridVoltage,
+        productionCT: productionCT.value,
+        consumptionCT,
+        ratedCurrent,
+        isConnected
+      }
+      alert(configObject)
+    } catch (err) {
+      alert('Please check the configuration values before submitting')
+    }
+  }
 
   return (
     <div className="fill-parent is-flex tile is-vertical has-text-centered system-config pl-10 pr-10">
@@ -23,7 +50,10 @@ function SystemConfiguration() {
       <NetworkWidget />
       <RSEWidget />
       <div className="submit-config">
-        <button className="button is-primary is-uppercase">
+        <button
+          onClick={submitConfig}
+          className="button is-primary is-uppercase"
+        >
           {t('SUBMIT_CONFIG')}
         </button>
       </div>
