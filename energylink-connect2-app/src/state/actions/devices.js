04301c3a5694a40128c161cb07a394c5918e1698
fercervantesx <so4tune@hotmail.com>
2020-02-25 21:34:21 -0600

    Polling Candidates (#136)

--- energylink-connect2-app/src/state/actions/devices.js
+++ energylink-connect2-app/src/state/actions/devices.js
@@ -7,3 +7,9 @@ export const DISCOVER_ERROR = createAction('discover devices error')
 export const PUSH_CANDIDATES_INIT = createAction('PUSH_CANDIDATES_INIT')
 export const PUSH_CANDIDATES_SUCCESS = createAction('PUSH_CANDIDATES_SUCCESS')
 export const PUSH_CANDIDATES_ERROR = createAction('PUSH_CANDIDATES_ERROR')
+export const FETCH_CANDIDATES_INIT = createAction('FETCH_CANDIDATES_INIT')
+export const FETCH_CANDIDATES_UPDATE = createAction('FETCH_CANDIDATES_UPDATE')
+export const FETCH_CANDIDATES_ERROR = createAction('FETCH_CANDIDATES_ERROR')
+export const FETCH_CANDIDATES_COMPLETE = createAction(
+  'FETCH_CANDIDATES_COMPLETE'
+)
---
+++ energylink-connect2-app/src/state/epics/devices/pollCandidates.js
@@ -0,0 +1,41 @@
+import { path, pathOr } from 'ramda'
+import { ofType } from 'redux-observable'
+import { from, of, timer } from 'rxjs'
+import { catchError, switchMap, takeUntil } from 'rxjs/operators'
+import { getApiPVS } from 'shared/api'
+import {
+  FETCH_CANDIDATES_UPDATE,
+  FETCH_CANDIDATES_COMPLETE,
+  FETCH_CANDIDATES_ERROR,
+  PUSH_CANDIDATES_SUCCESS
+} from 'state/actions/devices'
+
+const fetchCandidatesEpic = action$ => {
+  const stopPolling$ = action$.pipe(ofType(FETCH_CANDIDATES_COMPLETE.getType()))
+  return action$.pipe(
+    ofType(PUSH_CANDIDATES_SUCCESS.getType()),
+    switchMap(() =>
+      timer(0, 5000).pipe(
+        takeUntil(stopPolling$),
+        switchMap(() => {
+          const promise = getApiPVS()
+            .then(path(['apis', 'candidates']))
+            .then(api => api.getCandidates())
+          return from(promise).pipe(
+            switchMap(async response => {
+              const candidatesList = pathOr(
+                [],
+                ['body', 'candidates'],
+                response
+              )
+              return FETCH_CANDIDATES_UPDATE(candidatesList)
+            }),
+            catchError(error => of(FETCH_CANDIDATES_ERROR(error)))
+          )
+        })
+      )
+    )
+  )
+}
+
+export default fetchCandidatesEpic
