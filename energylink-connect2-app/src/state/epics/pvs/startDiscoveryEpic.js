7b98ec522f6eb38b7cd6892942b5424b72ace4bf
Fernando Cervantes <fernando.cervantes@wizeline.com>
2020-03-04 18:11:23 -0600

    fix/StartDiscoverySwagger

--- energylink-connect2-app/src/state/epics/pvs/startDiscoveryEpic.js
+++ energylink-connect2-app/src/state/epics/pvs/startDiscoveryEpic.js
@@ -1,19 +1,19 @@
 import { ofType } from 'redux-observable'
 import { of, from } from 'rxjs'
 import { catchError, mergeMap, map } from 'rxjs/operators'
+import { path } from 'ramda'
 import * as pvsActions from 'state/actions/pvs'
+import { getApiPVS } from 'shared/api'
 
 export const startDiscoveryEpic = action$ =>
   action$.pipe(
     ofType(pvsActions.START_DISCOVERY_INIT.getType()),
     mergeMap(() => {
-      const promise = fetch(
-        process.env.REACT_APP_PVS_SELECTEDADDRESS + '/dl_cgi/discovery',
-        {
-          method: 'post',
-          body: JSON.stringify({ Device: 'allnomi' })
-        }
-      )
+      const promise = getApiPVS()
+        .then(path(['apis', 'discovery']))
+        .then(api =>
+          api.discover({ id: 1 }, { requestBody: { Device: 'allnomi' } })
+        )
 
       return from(promise).pipe(
         map(response =>
