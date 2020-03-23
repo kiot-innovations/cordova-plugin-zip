36b6694d1287210594ef0f2bc116f8179bbb0d46
Vladimir Lopez <vladimir.lopez@wizeline.com>
2020-03-23 17:57:20 -0600

    Update the clean script

--- clean_arches.sh
+++ clean_arches.sh
@@ -1,35 +1,28 @@
+APP_PATH="${TARGET_BUILD_DIR}/${WRAPPER_NAME}"
+
 # This script loops through the frameworks embedded in the application and
 # removes unused architectures.
 find "$APP_PATH" -name '*.framework' -type d | while read -r FRAMEWORK
 do
     FRAMEWORK_EXECUTABLE_NAME=$(defaults read "$FRAMEWORK/Info.plist" CFBundleExecutable)
     FRAMEWORK_EXECUTABLE_PATH="$FRAMEWORK/$FRAMEWORK_EXECUTABLE_NAME"
-
-    if [ ! -f "${FRAMEWORK_EXECUTABLE_PATH}" ]; then
-        continue
-    fi
-
-    if xcrun lipo -info "${FRAMEWORK_EXECUTABLE_PATH}" | grep --silent "Non-fat"; then
-        echo "Framework non-fat, skipping: $FRAMEWORK_EXECUTABLE_NAME"
-        continue
-    fi
-
-    echo "Thinning framework $FRAMEWORK_EXECUTABLE_NAME"
+    echo "Executable is $FRAMEWORK_EXECUTABLE_PATH"
 
     EXTRACTED_ARCHS=()
 
     for ARCH in $ARCHS
     do
         echo "Extracting $ARCH from $FRAMEWORK_EXECUTABLE_NAME"
-        xcrun lipo -extract "$ARCH" "$FRAMEWORK_EXECUTABLE_PATH" -o "$FRAMEWORK_EXECUTABLE_PATH-$ARCH"
+        lipo -extract "$ARCH" "$FRAMEWORK_EXECUTABLE_PATH" -o "$FRAMEWORK_EXECUTABLE_PATH-$ARCH"
         EXTRACTED_ARCHS+=("$FRAMEWORK_EXECUTABLE_PATH-$ARCH")
     done
 
     echo "Merging extracted architectures: ${ARCHS}"
-    xcrun lipo -o "$FRAMEWORK_EXECUTABLE_PATH-merged" -create "${EXTRACTED_ARCHS[@]}"
+    lipo -o "$FRAMEWORK_EXECUTABLE_PATH-merged" -create "${EXTRACTED_ARCHS[@]}"
     rm "${EXTRACTED_ARCHS[@]}"
 
     echo "Replacing original executable with thinned version"
     rm "$FRAMEWORK_EXECUTABLE_PATH"
     mv "$FRAMEWORK_EXECUTABLE_PATH-merged" "$FRAMEWORK_EXECUTABLE_PATH"
+
 done
