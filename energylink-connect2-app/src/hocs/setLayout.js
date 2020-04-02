c183855840121a0ebb413ec0453c8931450b6ef0
Vladimir Lopez <vladimir.lopez@wizeline.com>
2020-04-02 13:42:15 -0600

    Workaround for footer

--- energylink-connect2-app/src/hocs/setLayout.js
+++ energylink-connect2-app/src/hocs/setLayout.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { propOr } from 'ramda'
 import { setHeader, setFooter } from 'state/actions/ui'
 import clsx from 'clsx'
 import './layout.scss'
@@ -16,6 +17,16 @@ const setLayout = (
       dispatch(setFooter(footer))
     }
   }, [dispatch])
+
+  useEffect(() => {
+    const customFooter = document.querySelector('.custom-footer')
+    const offSetTop = propOr(0, 'offsetTop', customFooter)
+    const clientHeight = propOr(0, 'clientHeight', document.body)
+
+    if (customFooter && clientHeight - offSetTop > 120)
+      customFooter.style.top = '100%'
+  })
+
   return (
     <div
       className={clsx({
