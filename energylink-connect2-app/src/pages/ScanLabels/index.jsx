f35be497fa89a7fa758be8c5d2be9c60a3661140
Vladimir Lopez <vladimir.lopez@wizeline.com>
2020-05-18 15:24:37 -0500

    Make matrix scann functions pure

--- energylink-connect2-app/src/pages/ScanLabels/index.jsx
+++ energylink-connect2-app/src/pages/ScanLabels/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useCallback } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { compose, map } from 'ramda'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
@@ -7,9 +7,15 @@ import { scanM } from 'shared/scandit'
 import { buildSN } from 'shared/utils'
 import { ADD_PVS_SN } from 'state/actions/pvs'
 import paths from 'routes/paths'
-import ErrorBoundary from 'components/Error'
 
 import './ScanLabels.scss'
+
+const addSN = dispatch => compose(dispatch, ADD_PVS_SN, buildSN)
+const addCodes = compose(map, addSN)
+const scanMatrix = compose(scanM, addCodes)
+const startScanning = dispatch => {
+  if (window.Scandit) return scanMatrix(dispatch)
+}
 
 function ScanDeviceLabels() {
   const t = useI18n()
@@ -19,20 +25,12 @@ function ScanDeviceLabels() {
 
   const onDone = useRef(null)
 
-  const addSN = compose(dispatch, ADD_PVS_SN, buildSN)
-  const addCodes = map(addSN)
-
   const finishedScanning = () => {
     if (typeof onDone.current === 'function') {
       onDone.current()
       history.push(paths.PROTECTED.SN_LIST.path)
     }
   }
-
-  const startScanning = useCallback(() => {
-    if (window.Scandit) onDone.current = scanM(addCodes)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   const triggerManualEntry = () => {
     history.push({
@@ -44,40 +42,37 @@ function ScanDeviceLabels() {
   }
 
   useEffect(() => {
-    startScanning()
-    return () => onDone.current()
-  }, [startScanning])
+    onDone.current = startScanning(dispatch)
+  }, [dispatch])
 
   return (
-    <ErrorBoundary>
-      <div className="scan-labels is-vertical has-text-centered pl-10 pr-10">
-        <span className="is-uppercase has-text-weight-bold mb-10">
-          {t('SCAN_EQUIPMENT')}
-        </span>
+    <div className="scan-labels is-vertical has-text-centered pl-10 pr-10">
+      <span className="is-uppercase has-text-weight-bold mb-10">
+        {t('SCAN_EQUIPMENT')}
+      </span>
 
-        <div id="scandit" />
+      <div id="scandit" />
 
-        <div className="hint-text mt-15 pl-15 pr-15">{t('BULK_SCAN_HINT')}</div>
+      <div className="hint-text mt-15 pl-15 pr-15">{t('BULK_SCAN_HINT')}</div>
 
-        <div className="has-text-white mt-10">
-          {t('FOUND_SN', serialNumbers.length)}
-        </div>
-
-        <button
-          className="button is-primary trigger-scan mt-15"
-          onClick={finishedScanning}
-        >
-          {t('DONE')}
-        </button>
-
-        <button
-          className="button has-text-centered is-uppercase is-secondary has-no-border"
-          onClick={triggerManualEntry}
-        >
-          {t('SN_MANUAL_ENTRY')}
-        </button>
+      <div className="has-text-white mt-10">
+        {t('FOUND_SN', serialNumbers.length)}
       </div>
-    </ErrorBoundary>
+
+      <button
+        className="button is-primary trigger-scan mt-15"
+        onClick={finishedScanning}
+      >
+        {t('DONE')}
+      </button>
+
+      <button
+        className="button has-text-centered is-uppercase is-secondary has-no-border"
+        onClick={triggerManualEntry}
+      >
+        {t('SN_MANUAL_ENTRY')}
+      </button>
+    </div>
   )
 }
 
