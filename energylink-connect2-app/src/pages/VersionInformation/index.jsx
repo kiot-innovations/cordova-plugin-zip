371e3c70b0acfa159ceb22a2fb7a59c2f2de23d8
José Ríos <jose.rios@wizeline.com>
2020-06-04 17:21:42 -0500

    CM2-581 Add base styles to version info

--- energylink-connect2-app/src/pages/VersionInformation/index.jsx
+++ energylink-connect2-app/src/pages/VersionInformation/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { useI18n } from 'shared/i18n'
 import appVersion from '../../macros/appVersion.macro'
+import Logo from '@sunpower/sunpowerimage'
+import './VersionInformation.scss'
 
 function VersionInformation() {
   const t = useI18n()
 
   return (
-    <section className="is-flex tile is-vertical section pt-0 fill-parent">
+    <section className="version-info is-flex tile is-vertical pt-0 pr-10 pl-10 full-height">
       <h1 className="has-text-centered is-uppercase has-text-weight-bold pb-20">
         {t('VERSION_INFORMATION')}
       </h1>
-      <h1 className="has-text-centered is-uppercase has-text-weight-bold pb-20">
-        {appVersion()}
-      </h1>
+
+      <div className="container">
+        <div className="logo">
+          <Logo />
+        </div>
+
+        <div className="data">{appVersion()}</div>
+      </div>
     </section>
   )
 }
