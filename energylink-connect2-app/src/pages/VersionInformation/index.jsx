af596469d2a03dc7e396d19959d69ec4344fbf32
José Ríos <jose.rios@wizeline.com>
2020-06-05 12:18:52 -0500

    CM2-581 Finish style refinements

--- energylink-connect2-app/src/pages/VersionInformation/index.jsx
+++ energylink-connect2-app/src/pages/VersionInformation/index.jsx
@@ -8,22 +8,22 @@ function VersionInformation() {
   const t = useI18n()
 
   return (
-    <section className="version-info is-flex tile is-vertical pt-0 pr-10 pl-10 full-height">
-      <h1 className="has-text-centered is-uppercase has-text-weight-bold pb-20">
+    <section className="version-info is-flex tile is-vertical has-text-weight-bold pt-0 pr-10 pl-10 full-height">
+      <h1 className="has-text-centered is-uppercase pb-20">
         {t('VERSION_INFORMATION')}
       </h1>
 
       <div className="main-container">
         <div className="logo-container has-text-centered">
           <Logo className="logo" />
-          <span className="is-uppercase has-text-white is-bold pt-20">
+          <span className="is-uppercase has-text-white pt-20">
             {t('APP_NAME')}
           </span>
         </div>
 
         <div className="data has-text-white has-text-centered">
-          <span className="has-text-white is-bold pb-10">{t('VERSION')}</span>
-          <span className="is-size-4 is-bold">{appVersion()}</span>
+          <span className="has-text-white pb-10">{t('VERSION')}</span>
+          <span className="is-size-4">{appVersion()}</span>
         </div>
       </div>
     </section>
