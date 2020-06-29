ce5652ebe534cb3e2acbc6ef98253b6a0611da6c
José Ríos <jose.rios@wizeline.com>
2020-06-29 14:46:20 -0500

    CM2-547 Complete device mapping error page

--- energylink-connect2-app/src/pages/ESSDeviceMappingError/index.js
+++ energylink-connect2-app/src/pages/ESSDeviceMappingError/index.js
@@ -6,10 +6,29 @@ function DeviceMappingError() {
   const t = useI18n()
 
   return (
-    <section className="version-info is-flex tile is-vertical has-text-weight-bold pt-0 pr-10 pl-10 full-height">
+    <section className="ess-device-mapping-error is-flex tile is-vertical pt-0 pr-10 pl-10 full-height">
       <h1 className="has-text-centered is-uppercase pb-20">
         {t('DEVICE_MAPPING')}
       </h1>
+
+      <div className="main-container">
+        <div className="pt-20 pb-20">
+          <span className="sp-close is-size-1 has-text-white" />
+        </div>
+        <div>{t('DEVICE_MAPPING_ERROR')}</div>
+      </div>
+
+      <div className="has-text-centered pr-20 pl-20">
+        {t('DEVICE_MAPPING_ERROR_ADVICE')}
+      </div>
+      <div className="are-small mt-20">
+        <button
+          className="button auto is-uppercase is-secondary"
+          onClick={() => {}}
+        >
+          {t('RETRY')}
+        </button>
+      </div>
     </section>
   )
 }
