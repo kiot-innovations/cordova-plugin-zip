a772ab650f9b80544e12526667e397b7d5da958b
José Ríos <jose.rios@wizeline.com>
2020-06-29 17:11:10 -0500

    CM2-547 Complete device mapping page

--- energylink-connect2-app/src/pages/ESSDeviceMapping/index.js
+++ energylink-connect2-app/src/pages/ESSDeviceMapping/index.js
@@ -6,10 +6,21 @@ function DeviceMapping() {
   const t = useI18n()
 
   return (
-    <section className="version-info is-flex tile is-vertical has-text-weight-bold pt-0 pr-10 pl-10 full-height">
+    <section className="ess-device-mapping is-flex tile is-vertical has-text-weight-bold pt-0 pr-10 pl-10 full-height">
       <h1 className="has-text-centered is-uppercase pb-20">
         {t('DEVICE_MAPPING')}
       </h1>
+
+      <div className="main-container">
+        <div className="pt-20 pb-20">
+          <div className="inline-loader">
+            <div className="ball-scale-ripple">
+              <div />
+            </div>
+          </div>
+        </div>
+        <div>{t('DEVICE_MAPPING_IN_PROGRESS')}</div>
+      </div>
     </section>
   )
 }
