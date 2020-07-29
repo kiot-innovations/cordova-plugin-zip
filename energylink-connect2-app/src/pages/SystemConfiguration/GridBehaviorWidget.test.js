c64436358331ffbce97959be21b91b37ab24e679
José Ríos <jose.rios@wizeline.com>
2020-07-29 16:58:01 -0500

    CM2-766 Add test for measured grid voltage of 240

--- energylink-connect2-app/src/pages/SystemConfiguration/GridBehaviorWidget.test.js
+++ energylink-connect2-app/src/pages/SystemConfiguration/GridBehaviorWidget.test.js
@@ -6,7 +6,7 @@ import GridBehaviorWidget from './GridBehaviorWidget'
 describe('GridBehaviorWidget', () => {
   let dispatchMock
 
-  let initialState = {
+  const gridVoltageZero = {
     systemConfiguration: {
       gridBehavior: {
         selectedOptions: {
@@ -37,6 +37,37 @@ describe('GridBehaviorWidget', () => {
     site: {}
   }
 
+  const gridVoltage240 = {
+    systemConfiguration: {
+      gridBehavior: {
+        selectedOptions: {
+          gridVoltage: 240,
+          profile: {
+            selfsupply: false,
+            zipcodes: [
+              {
+                max: 96162,
+                min: 90001
+              }
+            ],
+            filename: 'cb8e4e2f.meta',
+            id: 'cb8e4e2f365c5cd1434dd678566a208f0c866661',
+            name: 'CA CPUC R21 Reactive Power Priority'
+          },
+          lazyGridProfile: 0,
+          exportLimit: -1
+        },
+        profiles: [],
+        gridVoltage: {
+          grid_voltage: 240,
+          measured: 240,
+          selected: 0
+        }
+      }
+    },
+    site: {}
+  }
+
   beforeEach(() => {
     dispatchMock = jest.fn()
     jest.spyOn(reactRedux, 'useDispatch').mockImplementation(() => dispatchMock)
@@ -49,7 +80,14 @@ describe('GridBehaviorWidget', () => {
 
   test('renders correctly with grid voltage measured equals to 0', () => {
     const { component } = mountWithProvider(<GridBehaviorWidget />)(
-      initialState
+      gridVoltageZero
+    )
+    expect(component).toMatchSnapshot()
+  })
+
+  test('renders correctly with grid voltage measured equals to 240', () => {
+    const { component } = mountWithProvider(<GridBehaviorWidget />)(
+      gridVoltage240
     )
     expect(component).toMatchSnapshot()
   })
