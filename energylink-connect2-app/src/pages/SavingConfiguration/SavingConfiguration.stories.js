6b1076583f188abef62da23628268dbf0ba98744
José Ríos <jose.rios@wizeline.com>
2020-08-05 12:28:29 -0500

    CM2-872 Add Storybook story for saving configuration page

---
+++ energylink-connect2-app/src/pages/SavingConfiguration/SavingConfiguration.stories.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { configureStore } from 'state/store'
+import { Provider } from 'react-redux'
+
+import SavingConfiguration from './index'
+
+storiesOf('Saving Configuration Page', module)
+  .add('Successful Configuration', () => {
+    const { store } = configureStore({
+      systemConfiguration: { submit: { commissioned: true, error: '' } }
+    })
+
+    return (
+      <div className="full-min-height">
+        <Provider store={store}>
+          <SavingConfiguration />
+        </Provider>
+      </div>
+    )
+  })
+  .add('Unsuccessful Configuration', () => {
+    const { store } = configureStore({
+      systemConfiguration: {
+        submit: { commissioned: false, error: 'Something wrong happened.' }
+      }
+    })
+
+    return (
+      <div className="full-min-height">
+        <Provider store={store}>
+          <SavingConfiguration />
+        </Provider>
+      </div>
+    )
+  })
