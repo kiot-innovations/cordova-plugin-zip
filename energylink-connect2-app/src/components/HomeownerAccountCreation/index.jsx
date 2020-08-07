225aff153c10c1bb477960ed9c7456c90cae416b
José Ríos <jose.rios@wizeline.com>
2020-08-06 22:27:03 -0500

    CM2-872 Use form data to create fill in the email template
    Use the homeowner's email address to send the email, also fill in the
    first and last name using the provided data.

--- energylink-connect2-app/src/components/HomeownerAccountCreation/index.jsx
+++ energylink-connect2-app/src/components/HomeownerAccountCreation/index.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useI18n } from 'shared/i18n'
 import SwipeableBottomSheet from 'react-swipeable-bottom-sheet'
 import './HomeownerAccountCreation.scss'
 
 const HomeownerAccountCreation = ({ open, onChange }) => {
   const t = useI18n()
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
 
   return (
     <SwipeableBottomSheet open={open} onChange={onChange}>
@@ -12,22 +15,34 @@ const HomeownerAccountCreation = ({ open, onChange }) => {
         <span className="has-text-weight-bold">
           {t('CREATE_HOMEOWNER_ACCOUNT')}
         </span>
-        <input type="text" placeholder={t('FIRST_NAME')} onChange={() => {}} />
-        <input type="text" placeholder={t('LAST_NAME')} onChange={() => {}} />
+        <input
+          type="text"
+          placeholder={t('FIRST_NAME')}
+          onChange={event => setFirstName(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder={t('LAST_NAME')}
+          onChange={event => setLastName(event.target.value)}
+        />
         <input
           type="text"
           placeholder={t('HOMEOWNER_EMAIL')}
-          onChange={() => {}}
+          onChange={event => setEmail(event.target.value)}
         />
         <div className="mb-20">
           <button
             className="button is-primary is-uppercase"
             onClick={() =>
               window.open(
-                `mailto:pepe@example.com?subject=${t(
+                `mailto:${email}?subject=${t(
                   'HOMEOWNER_ACCOUNT_EMAIL_SUBJECT'
                 )}&body=${encodeURIComponent(
-                  t('HOMEOWNER_ACCOUNT_EMAIL_BODY_TEMPLATE')
+                  t(
+                    'HOMEOWNER_ACCOUNT_EMAIL_BODY_TEMPLATE',
+                    firstName,
+                    lastName
+                  )
                 )}`,
                 '_system'
               )
