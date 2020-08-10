2439e1c572e6023079f8a09a6060bedbb11ec160
Vladimir Lopez <vladimir.lopez@wizeline.com>
2020-08-10 12:34:17 -0500

    Bold message

--- energylink-connect2-app/src/pages/ErrorListGeneric/index.jsx
+++ energylink-connect2-app/src/pages/ErrorListGeneric/index.jsx
@@ -22,7 +22,9 @@ const ErrorComponent = ({ title, code, errorInfo, t }) => {
         <p className="error-code"> {t('ERROR_CODE', code)}</p>
         {either(
           not(`${code}`.startsWith('1')),
-          <p className="has-text-primary"> {t('FIX_ERROR_TO_PROCEED')}</p>
+          <p className="has-text-primary has-text-weight-bold">
+            {t('FIX_ERROR_TO_PROCEED')}
+          </p>
         )}
       </div>
       <div>
