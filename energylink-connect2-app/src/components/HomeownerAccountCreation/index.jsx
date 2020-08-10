6bdb291d735c79d904a13e31353d17bba8c77536
José Ríos <jose.rios@wizeline.com>
2020-08-10 12:19:58 -0500

    CM2-872 Use react-final-form-hooks library

--- energylink-connect2-app/src/components/HomeownerAccountCreation/index.jsx
+++ energylink-connect2-app/src/components/HomeownerAccountCreation/index.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useField, useForm } from 'react-final-form-hooks'
 import SwipeableBottomSheet from 'react-swipeable-bottom-sheet'
 import { isEmpty } from 'ramda'
+import TextField from '@sunpower/textfield'
 import { useI18n } from 'shared/i18n'
 import { createExternalLinkHandler } from 'shared/routing'
 import './HomeownerAccountCreation.scss'
 
 const HomeownerAccountCreation = ({ open, onChange, pvs }) => {
   const t = useI18n()
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const sendEmail = `mailto:${email}?subject=${t(
-    'HOMEOWNER_ACCOUNT_EMAIL_SUBJECT'
-  )}&body=${encodeURIComponent(
-    t('HOMEOWNER_ACCOUNT_EMAIL_BODY_TEMPLATE', firstName, lastName, pvs)
-  )}`
+  const onSubmit = ({ firstName, lastName, email }) =>
+    createExternalLinkHandler(
+      `mailto:${email}?subject=${t(
+        'HOMEOWNER_ACCOUNT_EMAIL_SUBJECT'
+      )}&body=${encodeURIComponent(
+        t('HOMEOWNER_ACCOUNT_EMAIL_BODY_TEMPLATE', firstName, lastName, pvs)
+      )}`
+    )()
+  const { form, handleSubmit } = useForm({
+    onSubmit
+  })
+  const firstName = useField('firstName', form)
+  const lastName = useField('lastName', form)
+  const email = useField('email', form)
+  const submitDisabled =
+    isEmpty(firstName.input.value) ||
+    isEmpty(lastName.input.value) ||
+    isEmpty(email.input.value)
 
   return (
     <SwipeableBottomSheet open={open} onChange={onChange}>
@@ -22,30 +34,36 @@ const HomeownerAccountCreation = ({ open, onChange, pvs }) => {
         <span className="has-text-weight-bold">
           {t('CREATE_HOMEOWNER_ACCOUNT')}
         </span>
-        <input
-          type="text"
-          placeholder={t('FIRST_NAME')}
-          onChange={event => setFirstName(event.target.value)}
-        />
-        <input
-          type="text"
-          placeholder={t('LAST_NAME')}
-          onChange={event => setLastName(event.target.value)}
-        />
-        <input
-          type="email"
-          placeholder={t('HOMEOWNER_EMAIL')}
-          onChange={event => setEmail(event.target.value)}
-        />
-        <div className="mb-20">
-          <button
-            disabled={isEmpty(firstName) || isEmpty(lastName) || isEmpty(email)}
-            className="button is-primary is-uppercase"
-            onClick={createExternalLinkHandler(sendEmail)}
-          >
-            {t('SEND_EMAIL')}
-          </button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            className="mt-20"
+            input={firstName.input}
+            meta={firstName.meta}
+            type="text"
+            placeholder={t('FIRST_NAME')}
+          />
+          <TextField
+            input={lastName.input}
+            meta={lastName.meta}
+            type="text"
+            placeholder={t('LAST_NAME')}
+          />
+          <TextField
+            input={email.input}
+            meta={email.meta}
+            type="email"
+            placeholder={t('HOMEOWNER_EMAIL')}
+          />
+          <div className="mt-20 mb-20">
+            <button
+              disabled={submitDisabled}
+              className="button is-primary is-uppercase"
+              type="submit"
+            >
+              {t('SEND_EMAIL')}
+            </button>
+          </div>
+        </form>
       </div>
     </SwipeableBottomSheet>
   )
