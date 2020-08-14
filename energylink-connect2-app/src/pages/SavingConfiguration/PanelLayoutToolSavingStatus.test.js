bc58516dff8c76ea23d1e6c8bdc227be6c1168aa
Matias <matias@analyticsfire.com>
2020-08-14 10:54:43 -0300

    [CM2-860] Saving panel layout tool after commission is success (#490)
    * [CM2-860] Saving panel layout tool after commission is success
    
    * [CM2-860] Added storybook for PanelLayoutToolSavingStatus

---
+++ energylink-connect2-app/src/pages/SavingConfiguration/PanelLayoutToolSavingStatus.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import * as reactRedux from 'react-redux'
+import PanelLayoutToolSavingStatus from './PanelLayoutToolSavingStatus'
+import * as i18n from 'shared/i18n'
+
+describe('PanelLayoutToolSavingStatus component', () => {
+  let dispatchMock
+
+  const initialState = {
+    pltWizard: {
+      saving: true,
+      saved: false,
+      error: ''
+    }
+  }
+
+  beforeEach(() => {
+    dispatchMock = jest.fn()
+    jest.spyOn(reactRedux, 'useDispatch').mockImplementation(() => dispatchMock)
+    jest.spyOn(reactRedux, 'useSelector').mockImplementation(() => initialState)
+    jest
+      .spyOn(i18n, 'useI18n')
+      .mockImplementation(path => (key, ...params) =>
+        `${key.toUpperCase()} ${params.join('_')}`.trim()
+      )
+  })
+
+  test('renders correctly', () => {
+    const { component } = mountWithProvider(<PanelLayoutToolSavingStatus />)({})
+    expect(component).toMatchSnapshot()
+  })
+})
