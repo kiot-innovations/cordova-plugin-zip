484444856a7fd4556e9867916622c4b4ab3cf66b
Matias <matias@analyticsfire.com>
2020-09-03 14:45:37 -0300

    [CM2-1049] Show error when status call on health check page (#538)
    * [CM2-1049] Show error when status call on health check page
    
    * cm2-1049 Added global error component to EssErrordetected

--- energylink-connect2-app/src/components/ESSErrorDetected/index.jsx
+++ energylink-connect2-app/src/components/ESSErrorDetected/index.jsx
@@ -9,34 +9,52 @@ function ErrorDetected({
   warnings = 0,
   url = '',
   next = '',
+  globalError = '',
   onRetry
 }) {
   const t = useI18n()
-  if (number === 0 && warnings === 0) return null
+  if (number === 0 && warnings === 0 && !globalError) return null
   return (
     <div className="error-list-detected pl-20 pr-20">
-      <p className="has-text-primary has-text-weight-bold has-text-centered mb-10 is-size-6">
-        {t('ERRORS_DETECTED', number)} &nbsp;
-        {either(warnings > 0, t('WARNINGS_DETECTED', warnings))}
-      </p>
-      <p className="has-text-centered">{t('FIX_ERRORS')}</p>
-      <div className="is-flex level">
-        <Link
-          className="button is-primary is-uppercase mt-20 is-fullwidth mr-5"
-          to={url}
-        >
-          {t(number > 0 ? 'ERROR_LIST' : 'WARNING_LIST')}
-        </Link>
-        <button
-          className="button is-primary is-outlined is-uppercase mt-20 is-fullwidth ml-5"
-          onClick={onRetry}
-        >
-          {t('RETRY')}
-        </button>
-      </div>
+      {either(
+        number === 0 && warnings === 0,
+        <>
+          <p className="has-text-primary has-text-weight-bold has-text-centered mb-10 is-size-6">
+            {t('ERRORS_DETECTED', 1)}
+          </p>
+          <p className="has-text-centered">{globalError}</p>
+          <button
+            className="button is-primary is-outlined is-uppercase mt-20 is-fullwidth ml-5"
+            onClick={onRetry}
+          >
+            {t('RETRY')}
+          </button>
+        </>,
+        <>
+          <p className="has-text-primary has-text-weight-bold has-text-centered mb-10 is-size-6">
+            {t('ERRORS_DETECTED', number)} &nbsp;
+            {either(warnings > 0, t('WARNINGS_DETECTED', warnings))}
+          </p>
+          <p className="has-text-centered">{t('FIX_ERRORS')}</p>
+          <div className="is-flex level">
+            <Link
+              className="button is-primary is-uppercase mt-20 is-fullwidth mr-5"
+              to={url}
+            >
+              {t(number > 0 ? 'ERROR_LIST' : 'WARNING_LIST')}
+            </Link>
+            <button
+              className="button is-primary is-outlined is-uppercase mt-20 is-fullwidth ml-5"
+              onClick={onRetry}
+            >
+              {t('RETRY')}
+            </button>
+          </div>
+        </>
+      )}
 
       {either(
-        number === 0,
+        number === 0 && !globalError,
         <Link
           className="button is-primary is-outlined is-uppercase mt-10 is-fullwidth"
           to={next}
