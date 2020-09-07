fd4ba93624b832281da752af00c0d30a7eeb1354
Vladimir Lopez <vladimir.lopez@wizeline.com>
2020-09-07 15:26:56 -0500

    fix modal flickering

--- energylink-connect2-app/src/components/GlobalModal/AppUpdaterModal.jsx
+++ energylink-connect2-app/src/components/GlobalModal/AppUpdaterModal.jsx
@@ -1,15 +1,8 @@
-import React, { useCallback } from 'react'
-import { compose } from 'ramda'
-import { useDispatch } from 'react-redux'
-
+import React from 'react'
 import AppUpdater from 'components/AppUpdater'
-import { APP_UPDATE_OPEN_MARKET } from 'state/actions/global'
 
-function AppUpdaterModal() {
-  const dispatch = useDispatch()
-  const onUpdate = useCallback(compose(dispatch, APP_UPDATE_OPEN_MARKET), [])
-
-  return <AppUpdater onUpdate={onUpdate} />
+function AppUpdaterModal(props) {
+  return <AppUpdater {...props} />
 }
 
 export default AppUpdaterModal
