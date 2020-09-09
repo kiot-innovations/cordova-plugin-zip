f2188ca433b0438973663bea13a49fa2a5cade13
fercervantesx <so4tune@hotmail.com>
2020-09-09 17:25:32 -0500

    CM2-1071 Storage Error Footer Redesign (#567)

--- energylink-connect2-app/src/components/ESSErrorDetected/index.jsx
+++ energylink-connect2-app/src/components/ESSErrorDetected/index.jsx
@@ -32,19 +32,24 @@ function ErrorDetected({
         </>,
         <>
           <p className="has-text-primary has-text-weight-bold has-text-centered mb-10 is-size-6">
-            {t('ERRORS_DETECTED', number)} &nbsp;
-            {either(warnings > 0, t('WARNINGS_DETECTED', warnings))}
+            {either(number > 0, t('ERRORS_DETECTED', number))} &nbsp;
+            {either(
+              warnings > 0,
+              number > 0
+                ? t('WARNINGS_DETECTED', warnings)
+                : t('WARNINGS_ONLY', warnings)
+            )}
           </p>
           <p className="has-text-centered">{t('FIX_ERRORS')}</p>
           <div className="is-flex level">
             <Link
-              className="button is-primary is-uppercase mt-20 is-fullwidth mr-5"
+              className="button is-primary is-outlined is-uppercase mt-20 is-fullwidth mr-5"
               to={url}
             >
               {t(number > 0 ? 'ERROR_LIST' : 'WARNING_LIST')}
             </Link>
             <button
-              className="button is-primary is-outlined is-uppercase mt-20 is-fullwidth ml-5"
+              className="button is-primary is-uppercase mt-20 is-fullwidth ml-5"
               onClick={onRetry}
             >
               {t('RETRY')}
