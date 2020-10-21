a5cd5993092a18786105820561b8a67ac021ce95
Esteban Trillo <esteban.trillo@wizeline.com>
2020-10-21 11:31:07 -0500

    [1263] allow for the enqueue of the modals (#676)

---
+++ energylink-connect2-app/src/state/epics/modals/showModal.js
@@ -0,0 +1,21 @@
+import { ofType } from 'redux-observable'
+import { HIDE_MODAL, SET_CURRENT_MODAL, SHOW_MODAL } from 'state/actions/modal'
+import { concatMap, map, take } from 'rxjs/operators'
+import { concat, of } from 'rxjs'
+import { EMPTY_ACTION } from 'state/actions/share'
+
+const showModalEpic = action$ =>
+  action$.pipe(
+    ofType(SHOW_MODAL.getType()),
+    concatMap(({ payload }) =>
+      concat(
+        of(SET_CURRENT_MODAL(payload)),
+        action$.pipe(
+          ofType(HIDE_MODAL.getType()),
+          map(() => EMPTY_ACTION()),
+          take(1)
+        )
+      )
+    )
+  )
+export default [showModalEpic]
