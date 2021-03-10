57bc0c52b848f44673884d74af1cb0bf1b10345f
Alvin Cheung <alvinycheung@gmail.com>
2021-03-10 11:51:13 -0600

    Feat - [CM2-1600] - Fallback for missing FW build information (#1024)
    * [CM2-1600] - Fallback for missing FW build information
    
    * [CM2-1600] Fix build by prettyfing the all the things
    
    * [CM2-1600] Fix build by running all the tests
    
    Co-authored-by: Esteban Trillo <esteban@devtrillo.com>
    Co-authored-by: Esteban Trillo <esteban.trillo@wizeline.com>
    Co-authored-by: Leont Stepanov <leont.stepanov@sunpowercorp.com>

--- energylink-connect2-app/src/components/ReleaseNotes/index.jsx
+++ energylink-connect2-app/src/components/ReleaseNotes/index.jsx
@@ -1,50 +1,67 @@
 import React from 'react'
+import { isNil, pathOr, prop } from 'ramda'
 import moment from 'moment'
 import { useI18n } from 'shared/i18n'
 import Collapsible from 'components/Collapsible'
 import { createMarkup, either } from 'shared/utils'
+import { useSelector } from 'react-redux'
 
 import './ReleaseNotes.scss'
 
 const ReleaseNotes = ({ title, releaseNotes, currentVersion = null }) => {
   const t = useI18n()
-  const { versions } = releaseNotes
+  const versions = pathOr([], 'versions', releaseNotes)
+  const fwFileInfo = useSelector(pathOr('', ['fileDownloader', 'fileInfo']))
+  if (isNil(currentVersion)) {
+    currentVersion = prop('version', fwFileInfo)
+  }
 
   return (
-    <section className="release-notes is-flex tile is-vertical has-text-weight-bold pt-0 pr-10 pl-10 full-height">
+    <section className="release-notes is-flex tile is-vertical pt-0 pr-10 pl-10 full-height">
       <h1 className="has-text-centered is-uppercase pb-20">{t(title)}</h1>
 
-      <div className="body has-text-white">
-        {versions.map((version, index) => {
-          const versionNumber = version.versionNumber
-          const title = `${t('VERSION')} ${versionNumber}`
-          const isFirstVersionInList = index === 0
-          const { releaseDate, bodyMarkdown } = version
-          return (
-            <div className="version" key={index}>
-              {//show the previous release notes title
-              //above the second set of release notes.
-              index === 1 && (
-                <div className="previous-release-notes">
-                  {t('PREVIOUS_RELEASE_NOTES')}
-                </div>
-              )}
-              <div className="mb-10">
-                <Collapsible title={title} expanded={isFirstVersionInList}>
-                  <div className="releaseDate">
-                    {t('RELEASE_DATE')}{' '}
-                    {moment(releaseDate).format('MM/DD/YYYY')}
+      {either(
+        versions.length > 0,
+        <div className="body has-text-white">
+          {versions.map((version, index) => {
+            const versionNumber = version.versionNumber
+            const title = `${t('VERSION')} ${versionNumber}`
+            const isFirstVersionInList = index === 0
+            const { releaseDate, bodyMarkdown } = version
+            return (
+              <div className="version" key={index}>
+                {//show the previous release notes title
+                //above the second set of release notes.
+                index === 1 && (
+                  <div className="previous-release-notes">
+                    {t('PREVIOUS_RELEASE_NOTES')}
                   </div>
-                  <div
-                    className="content"
-                    dangerouslySetInnerHTML={createMarkup(bodyMarkdown)}
-                  />
-                </Collapsible>
+                )}
+                <div className="mb-10">
+                  <Collapsible title={title} expanded={isFirstVersionInList}>
+                    <div className="releaseDate">
+                      {t('RELEASE_DATE')}{' '}
+                      {moment(releaseDate).format('MM/DD/YYYY')}
+                    </div>
+                    <div
+                      className="content"
+                      dangerouslySetInnerHTML={createMarkup(bodyMarkdown)}
+                    />
+                  </Collapsible>
+                </div>
               </div>
-            </div>
-          )
-        })}
-      </div>
+            )
+          })}
+        </div>,
+        <div className="body has-text-white">
+          <div className="collapsible">
+            <h1 class="mb-10">
+              {t('CURRENT_VERSION')}: {currentVersion}
+            </h1>
+            <p>{t('UNABLE_TO_RETRIEVE_RELEASE_NOTES')}</p>
+          </div>
+        </div>
+      )}
 
       {either(
         currentVersion,
