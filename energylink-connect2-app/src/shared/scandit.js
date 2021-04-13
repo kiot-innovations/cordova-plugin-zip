cdd5a28b880c7428c015e27bed4a7f9914af2a51
Alvin Cheung <alvinycheung@gmail.com>
2021-04-13 18:51:26 -0500

    Feature/cm2 1950/scandit (#1107)
    * [CM2-1950] - Scandit upgrade to 6.7.2 - WIP
    
    * [CM2-1950] - Scandit upgrade to remove AR functionality
    
    * Update energylink-connect2-app/src/shared/scandit.js
    
    Co-authored-by: fercervantesx <so4tune@hotmail.com>
    
    * Update energylink-connect2-app/src/shared/scandit.js
    
    Co-authored-by: fercervantesx <so4tune@hotmail.com>
    
    * CM2-1950 Bumped scandit-cordova-datacapture-core/barcode versions in config.xml
    
    * CM2-1950 Trying to remove cached gradle folder to see if it works
    
    * CM2-1950 Added back Restore cached Gradle folder step on quick-build
    
    * CM2-1950 Removed restoring cache steps on quick-build
    
    * [CM2-1950] Step scandit back to 6.7.1
    
    * [CM2-1950] - Step scandit back to 6.7.1
    
    * CM2-1950 Ignore duplicate barcodes during Scandit session
    
    Co-authored-by: fercervantesx <so4tune@hotmail.com>

--- energylink-connect2-app/src/shared/scandit.js
+++ energylink-connect2-app/src/shared/scandit.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import * as Sentry from '@sentry/browser'
-import { path, map, contains, flip, reject } from 'ramda'
+import { path, contains, flip, reject } from 'ramda'
 import { isIos } from './utils'
 
 const fcontains = flip(contains)
@@ -34,35 +34,43 @@ export function scanM(onRecognize, nodeID = 'scandit') {
   // sample we enable a very generous set of symbologies. In your own app ensure that you only enable the
   // symbologies that your app requires as every additional enabled symbology has an impact on processing times.
   settings.enableSymbologies([Scandit.Symbology.Code128])
+  settings.codeDuplicateFilter = -1
 
   // Create new barcode tracking mode with the settings from above.
-  const barcodeTracking = Scandit.BarcodeTracking.forContext(context, settings)
+  const barcodeCapture = Scandit.BarcodeCapture.forContext(context, settings)
 
-  const feedback = new Scandit.Feedback(null, Scandit.Sound.defaultSound)
-  feedback.emit()
+  const feedback = new Scandit.Feedback(null, null)
+  barcodeCapture.feedback = feedback
 
   const history = []
 
+  // TODO: These color definitions don't do anything
+  const fill = Scandit.Color.fromRGBA(0, 255, 0, 0.3)
+  const stroke = Scandit.Color.fromRGBA(0, 255, 0, 1)
+  const brush = new Scandit.Brush(fill, stroke, 2)
+  barcodeCapture.brush = brush
+
   // Register a listener to get informed whenever a new barcode is tracked.
-  barcodeTracking.addListener({
-    didUpdateSession: (barcodeTracking, session) => {
-      if (session.addedTrackedBarcodes.length > 0) {
-        const barcodes = map(
-          path(['barcode', 'data']),
-          session.addedTrackedBarcodes
-        )
+  const barcodeCaptureListener = {
+    didScan: (barcodeCapture, session) => {
+      const newlyRecognizedBarcodes = session.newlyRecognizedBarcodes
+
+      if (newlyRecognizedBarcodes.length > 0) {
+        const barcodes = newlyRecognizedBarcodes.map(barcode => {
+          return path(['data'], barcode)
+        })
 
         const newlyAdded = reject(fcontains(history), barcodes)
-
         newlyAdded.forEach(ab => {
           history.push(ab)
-          feedback.emit()
         })
 
         onRecognize(barcodes)
       }
     }
-  })
+  }
+
+  barcodeCapture.addListener(barcodeCaptureListener)
 
   // To visualize the on-going barcode tracking process on screen, setup a data capture view that renders the
   // camera preview. The view must be connected to the data capture context.
@@ -73,29 +81,17 @@ export function scanM(onRecognize, nodeID = 'scandit') {
 
   // Add a barcode tracking overlay to the data capture view to render the location of captured barcodes on top of
   // the video preview. This is optional, but recommended for better visual feedback.
-  const overlay = Scandit.BarcodeTrackingBasicOverlay.withBarcodeTrackingForView(
-    barcodeTracking,
-    view
-  )
-
-  const fill = Scandit.Color.fromRGBA(0, 255, 0, 0.3)
-  const stroke = Scandit.Color.fromRGBA(0, 255, 0, 1)
-
-  overlay.listener = {
-    brushForTrackedBarcode: (overlay, trackedBarcode) =>
-      new Scandit.Brush(fill, stroke, 2)
-  }
+  Scandit.BarcodeCaptureOverlay.withBarcodeCaptureForView(barcodeCapture, view)
 
   // Switch camera on to start streaming frames and enable the barcode tracking mode.
   // The camera is started asynchronously and will take some time to completely turn on.
   const toOn = camera.switchToDesiredState(Scandit.FrameSourceState.On)
   toOn.catch(Sentry.captureException)
-  barcodeTracking.enabled = true
   node.textContent = Math.random()
 
   return () => {
     const toOff = camera.switchToDesiredState(Scandit.FrameSourceState.Off)
     toOff.catch(Sentry.captureException)
-    barcodeTracking.enabled = false
+    barcodeCapture.removeListener(barcodeCaptureListener)
   }
 }
