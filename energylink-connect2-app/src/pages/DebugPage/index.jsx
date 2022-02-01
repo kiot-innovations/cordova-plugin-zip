8141004bb0974dce29245fdb6f7011084371a1c4
Vladimir Lopez <vladimir.lopez@wizeline.com>
2022-02-01 12:48:23 -0600

    CM2-2775 Fix Debug page styles and refactored the pages list rendering (#1435)
    Co-authored-by: Leont Stepanov <leont.stepanov@sunpowercorp.com>

--- energylink-connect2-app/src/pages/DebugPage/index.jsx
+++ energylink-connect2-app/src/pages/DebugPage/index.jsx
@@ -21,11 +21,43 @@ import {
   SHOW_SUPERUSER_SETTINGS,
   HIDE_SUPERUSER_SETTINGS
 } from 'state/actions/superuser'
+import { MENU_HIDE } from 'state/actions/ui'
 
 import './DebugPage.scss'
 
-const renderDebugLink = (history, name, path) => (
-  <div className="debug-route mb-10" onClick={() => history.push(path)}>
+const debugPages = {
+  SystemChecks: paths.PROTECTED.SYSTEM_CHECKS.path,
+  'Saving Configuration': paths.PROTECTED.SAVING_CONFIGURATION.path,
+  'Live Data': paths.PROTECTED.DATA.path,
+  'Other Devices': paths.PROTECTED.ADD_STRING_INVERTERS.path,
+  'String Inverters Setup': paths.PROTECTED.CONFIGURE_STRING_INVERTER.path,
+  'Precommissioning Configs': paths.PROTECTED.PRECOMMISSIONING_CONFIGS.path,
+  'Scan Labels (Scandit)': paths.PROTECTED.SCAN_LABELS.path,
+  'SunVault PreDiscovery': paths.PROTECTED.STORAGE_PREDISCOVERY.path,
+  'SunVault Connected Device Update': paths.PROTECTED.EQS_UPDATE.path,
+  'SunVault Device Mapping': paths.PROTECTED.ESS_DEVICE_MAPPING.path,
+  'SunVault Health Check': paths.PROTECTED.ESS_HEALTH_CHECK.path,
+  'Connection Recovery Page': paths.PROTECTED.CONNECTION_LOST.path,
+  'RMA - Existing Devices for PVS Replacement':
+    paths.PROTECTED.RMA_EXISTING_DEVICES.path,
+  'RMA - New Inventory Count': paths.PROTECTED.RMA_INVENTORY.path,
+  'RMA - New and existing Microinverters': paths.PROTECTED.RMA_SN_LIST.path,
+  'RMA - Edit Devices': paths.PROTECTED.RMA_DEVICES.path,
+  'Standard Serial Number List': paths.PROTECTED.SN_LIST.path,
+  'Connect PVS to Internet': paths.PROTECTED.PVS_PROVIDE_INTERNET.path,
+  'Precommissioning Checklist': paths.PROTECTED.PRECOMM_CHECKLIST.path,
+  'POC for BLE Connection': paths.PROTECTED.NEARBY_PVS.path,
+  'Precommissioning Configurations':
+    paths.PROTECTED.PRECOMMISSIONING_CONFIGS.path,
+  'Microinverters Discovery': paths.PROTECTED.DEVICES.path,
+  'System Configuration': paths.PROTECTED.SYSTEM_CONFIGURATION.path
+}
+
+const renderDebugLink = (name, path, onDebugLinkClicked) => (
+  <div
+    className="debug-route is-flex mb-10"
+    onClick={() => onDebugLinkClicked(path)}
+  >
     <div className="route-name">
       <span>{name}</span>
     </div>
@@ -34,6 +66,9 @@ const renderDebugLink = (history, name, path) => (
     </div>
   </div>
 )
+
+const buildPageMappingFn = (debugPagesList, onDebugLinkClicked) => pageName =>
+  renderDebugLink(pageName, debugPagesList[pageName], onDebugLinkClicked)
 
 const DebugPage = () => {
   const t = useI18n()
@@ -45,6 +80,7 @@ const DebugPage = () => {
     doNotUpdatePVS
     // doNotUpdateESS
   } = useSelector(state => state.fileDownloader.settings)
+
   const [selectedESSUpdate, setSelectedESSUpdate] = useState(essUpdateOverride)
   const [selectedPVSUpdate, setSelectedPVSUpdate] = useState(pvsUpdateOverride)
   const [showThisMightNotWork, setShowThisMightNotWork] = useState(false)
@@ -77,6 +113,11 @@ const DebugPage = () => {
   useEffect(() => {
     dispatch(DOWNLOAD_SUPERUSER_FIRMWARE_LIST())
   }, [dispatch])
+
+  const onDebugLinkClicked = path => {
+    history.push(path)
+    dispatch(MENU_HIDE())
+  }
 
   const getPVSDisplayName = pvsURL => {
     const urlSplit = pvsURL.split('/')
@@ -125,7 +166,7 @@ const DebugPage = () => {
   }
 
   return (
-    <div className="pr-10 pl-10">
+    <div className="pr-10 pl-10 debug-page">
       <div className="has-text-centered mb-25">
         <span className="has-text-weight-bold title">
           {t('SUPERUSER_OPTIONS')}
@@ -170,7 +211,7 @@ const DebugPage = () => {
                 dispatch(SET_DO_NOT_UPDATE_PVS(setting))
                 dispatch(SHOW_SUPERUSER_SETTINGS())
               }}
-            ></Toggler>
+            />
           </div>
           <hr className="mb-10" />
           <b className="option-title">
@@ -210,116 +251,9 @@ const DebugPage = () => {
       </div>
       <div>
         <Collapsible title="Debug Routes" expanded={true}>
-          {renderDebugLink(
-            history,
-            'SystemChecks',
-            paths.PROTECTED.SYSTEM_CHECKS.path
-          )}
-          {renderDebugLink(
-            history,
-            'Saving Configuration',
-            paths.PROTECTED.SAVING_CONFIGURATION.path
-          )}
-          {renderDebugLink(history, 'Live Data', paths.PROTECTED.DATA.path)}
-          {renderDebugLink(
-            history,
-            'Other Devices',
-            paths.PROTECTED.ADD_STRING_INVERTERS.path
-          )}
-          {renderDebugLink(
-            history,
-            'String Inverters Setup',
-            paths.PROTECTED.CONFIGURE_STRING_INVERTER.path
-          )}
-          {renderDebugLink(
-            history,
-            'Precommissioning Configs',
-            paths.PROTECTED.PRECOMMISSIONING_CONFIGS.path
-          )}
-          {renderDebugLink(
-            history,
-            'Scan Labels (Scandit)',
-            paths.PROTECTED.SCAN_LABELS.path
-          )}
-          {renderDebugLink(
-            history,
-            'SunVault PreDiscovery',
-            paths.PROTECTED.STORAGE_PREDISCOVERY.path
-          )}
-          {renderDebugLink(
-            history,
-            'SunVault Connected Device Update',
-            paths.PROTECTED.EQS_UPDATE.path
-          )}
-          {renderDebugLink(
-            history,
-            'SunVault Device Mapping',
-            paths.PROTECTED.ESS_DEVICE_MAPPING.path
-          )}
-          {renderDebugLink(
-            history,
-            'SunVault Health Check',
-            paths.PROTECTED.ESS_HEALTH_CHECK.path
-          )}
-          {renderDebugLink(
-            history,
-            'Connection Recovery Page',
-            paths.PROTECTED.CONNECTION_LOST.path
-          )}
-          {renderDebugLink(
-            history,
-            'RMA - Existing Devices for PVS Replacement',
-            paths.PROTECTED.RMA_EXISTING_DEVICES.path
-          )}
-          {renderDebugLink(
-            history,
-            'RMA - New Inventory Count',
-            paths.PROTECTED.RMA_INVENTORY.path
-          )}
-          {renderDebugLink(
-            history,
-            'RMA - New and existing Microinverters',
-            paths.PROTECTED.RMA_SN_LIST.path
-          )}
-          {renderDebugLink(
-            history,
-            'RMA - Edit Devices',
-            paths.PROTECTED.RMA_DEVICES.path
-          )}
-          {renderDebugLink(
-            history,
-            'Standard Serial Number List',
-            paths.PROTECTED.SN_LIST.path
-          )}
-          {renderDebugLink(
-            history,
-            'Connect PVS to Internet',
-            paths.PROTECTED.PVS_PROVIDE_INTERNET.path
-          )}
-          {renderDebugLink(
-            history,
-            'Precommissioning Checklist',
-            paths.PROTECTED.PRECOMM_CHECKLIST.path
-          )}
-          {renderDebugLink(
-            history,
-            'POC for BLE Connection',
-            paths.PROTECTED.NEARBY_PVS.path
-          )}
-          {renderDebugLink(
-            history,
-            'Precommissioning Configurations',
-            paths.PROTECTED.PRECOMMISSIONING_CONFIGS.path
-          )}
-          {renderDebugLink(
-            history,
-            'Microinverters Discovery',
-            paths.PROTECTED.DEVICES.path
-          )}
-          {renderDebugLink(
-            history,
-            'System Configuration',
-            paths.PROTECTED.SYSTEM_CONFIGURATION.path
+          {map(
+            buildPageMappingFn(debugPages, onDebugLinkClicked),
+            Object.keys(debugPages)
           )}
         </Collapsible>
       </div>
