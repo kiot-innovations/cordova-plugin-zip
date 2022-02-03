b18794aecb9bc520ace59733f009237c0d6a92eb
Vladimir Lopez <vladimir.lopez@wizeline.com>
2022-02-02 18:14:34 -0600

    Improve sed statement that sets the ndkVersion (#1436)

--- hooks/setNDKVersion.sh
+++ hooks/setNDKVersion.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 OS=$(uname)
+nl=$'\n'
 if [ $OS = "Darwin" ]; then
-    sed -i"" -E 's/android {/android {\n ndkVersion "21.4.7075529"/' platforms/android/app/build.gradle
+    sed -i'' -E "s#   ndkVersion \"21.4.7075529\"##" platforms/android/app/build.gradle
+    sed -i'' -E "s#android {#android {\\${nl}   ndkVersion \"21.4.7075529\"#" platforms/android/app/build.gradle
 else
-    sed -i"" -E 's/android \{/android \{\n ndkVersion "21.4.7075529"/' platforms/android/app/build.gradle
+    sed -i'' -E "s#android \{#android \{\n ndkVersion \"21.4.7075529\"#" platforms/android/app/build.gradle
 fi
